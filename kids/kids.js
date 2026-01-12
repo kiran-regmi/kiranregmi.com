ee0eff15027eebfa2c7d313e736500a73e2d3cff
kiran-regmi <kiranimani@gmail.com>
2026-01-12 14:37:39 -0600

    update name

--- kids/kids.js
+++ kids/kids.js
@@ -13,7 +13,7 @@
   // Replace with your real session/user from kiranregmi.com
   // -------------------------
   const session = {
-    kidName: "Pranav!",
+    kidName: "Pranav",
     kidId: "kid_local_1",
   };
 
